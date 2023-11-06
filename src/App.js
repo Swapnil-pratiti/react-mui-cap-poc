b30bee54158c945f369f625251da6b505f81f355
SWAPNIL AKOLKAR <105718689+Swapnil-pratiti@users.noreply.github.com>
2023-11-06 17:20:07 +0530

    added card action area

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
@@ -18,6 +19,7 @@ const bull = (
 function App() {
   return (
     <Card sx={{ minWidth: 275, margin: "20px" }}>
+    <CardActionArea onClick={() => alert("card action area clicked")}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Word of the Day
@@ -37,6 +39,7 @@ function App() {
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
+      </CardActionArea>
     </Card>
   );
 }
